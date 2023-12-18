c8768151c8dd78aaa832b0b2951b62cc69f4d7ed
Stephan <ssomers42@gmail.com>
2023-12-17 23:06:40 -0800

    ... again

--- netlify/functions/connectDB.mjs
+++ netlify/functions/connectDB.mjs
@@ -11,9 +11,12 @@ export default () => {
       database: 'imdb',
     });
     // Return the credentials
-    return new Response(JSON.stringify({ connection }), {
-      status: 200,
-    });
+    return (
+      new Response(connection),
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.error(error);
 
